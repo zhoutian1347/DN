242d3cc135cfb05301b4d6a4e33062d0d3bdef5d
Administrator <Administrator@EMM>
2019-10-16 21:03:53 +0800

    1

--- Proxy/src/com/tedu/Proxy/Sss.java
+++ Proxy/src/com/tedu/Proxy/Sss.java
@@ -1,9 +1,28 @@
 package com.tedu.Proxy;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
 public class Sss {
 	public static void main(String[] args) {
 		ISinger target = new Singer();
-		ISinger proxy = new SingProxy(target);
+//		ISinger proxy = new SingProxy(target);
+//		proxy.sing();
+		
+		
+		ISinger proxy = (ISinger) Proxy.newProxyInstance(target.getClass().getClassLoader(), 
+				target.getClass().getInterfaces(), 
+				new InvocationHandler() {
+					
+					@Override
+					public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+						System.out.println("问好");
+						Object returnValue = method.invoke(target, args);
+						System.out.println("感谢");
+						return returnValue;
+					}
+				});
 		proxy.sing();
 	}
 }
